6b83ed65604d1448a3f4764587bf8ad0fada77d9
Lawrence Wu <llwu@llwu.me>
2021-10-06 20:51:43 -0700

    checkout endpoint

---
+++ src/checkout.js
@@ -0,0 +1,30 @@
+import { Stripe } from 'stripe-workers'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://nobleprize.com/',
+    cancel_url: 'https://nobleprize.com/',
+  })
+
+  return Response.redirect(session.url, 301)
+}
