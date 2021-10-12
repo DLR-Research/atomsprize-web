55277b841e9f543ab1a3a8120e63f9aad08831b7
Lawrence Wu <llwu@llwu.me>
2021-10-12 00:49:35 -0700

    Alias /stats routes

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,14 +2,19 @@ import { Router } from 'itty-router'
 
 import handleCheckout from './checkout'
 import handleShare from './share'
-import { handleStats, handleStatsIndex } from './stats'
+import { getStats, indexStats } from '../../campaigns-worker/src/stats'
 
 const router = Router()
 
 router.post('/checkout', handleCheckout)
 router.get('/share/:id', handleShare)
-router.get('/stats', handleStatsIndex)
-router.get('/stats/:id', handleStats)
+router.get('/stats', ({ url }) => indexStats({
+  url,
+  params: { campaignId: '1' },
+}))
+router.get('/stats/:id', (
+  { params: { id } } : { params: { id: string } }
+) => getStats({ params: { campaignId: '1', userId: id } }))
 router.all('*', () => new Response('404 Not Found', { status: 404 }))
 
 addEventListener('fetch', e => {
